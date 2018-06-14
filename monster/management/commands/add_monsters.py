cb240ddbef979af5e9d0cef6ff17d212b7ff7790
Sam Nicholls <sam.n@studio8media.co.uk>
2018-06-14 14:52:21 +0100

    Update add_monsters command

--- monster/management/commands/add_monsters.py
+++ monster/management/commands/add_monsters.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.files import File
 
+from datetime import datetime
+import os
+
 from monster import models
 
 class Command(BaseCommand):
@@ -13,7 +16,10 @@ class Command(BaseCommand):
         ref = open(options["filename"])
 
         last_reference = None
+        last_event = None
+        last_dir = ""
 
+        n = 1
         for line in ref:
             line = line.strip()
             fields = line.split("\t")
@@ -24,14 +30,26 @@ class Command(BaseCommand):
             elif fields[0] == "R":
                 last_reference = models.Reference.objects.get(pk=fields[1])
 
+            elif fields[0] == "E":
+                last_event = models.SequencingEvent(name=fields[1], description=fields[2], date=datetime.strptime(fields[3], "%Y-%m-%d"), location=fields[4])
+                last_event.save()
+
+            elif fields[0] == "D":
+                last_dir = fields[1]
+
             else:
                 if not last_reference:
                     raise CommandError("oh no")
 
-                m = models.Monster(name=fields[0], reference=last_reference, scientist_name=fields[2], institute_name=fields[3])
+                name = list(fields[0])
+                name[0] = name[0].upper()
+                name = "".join(name)
+                m = models.Monster(name=name, reference=last_reference, scientist_name=fields[2], institute_name=fields[3], event=last_event, number=n)
                 m.save()
-                m.record_image.save(fields[4].split("/")[-1], File(open(fields[4])))
+                f_path = os.path.join(last_dir, fields[4])
+                m.record_image.save(os.path.basename(f_path), File(open(f_path)))
                 m.annotate(fields[1])
                 m.save()
                 print(m.id)
+                n += 1
 
